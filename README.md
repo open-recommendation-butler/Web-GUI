9f0578dca810bec35d03bc210d737db1d72b132f
matthmeyer <maetthy@arcor.de>
2023-02-16 11:36:49 +0100

    Copy code from ORB repository

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Web-GUI
 This is the web GUI for Open Recommendation Butler. It's a light React frontend to use in combination with the powerful search and recommendation backend ORB. 
 
+## Available Scripts
+
+In the project directory, you can run:
 
 ### `npm start`
 
---
+++ src/helpers/requests.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// let DOMAIN = "http://localhost:8000";
+let DOMAIN = "https://open-recommendation-butler.tech/api";
+
+export async function GET(path) {
+  return axios.get(`${DOMAIN}${path}`)
+}
+
+export async function POST(path, data=null) {
+  let headers = {};
+  return axios.post(`${DOMAIN}${path}`, data)
+}
+
+export async function DELETE(path) {
+  return axios.delete(`${DOMAIN}${path}`)
+}
